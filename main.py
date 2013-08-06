04bcf3ae8c7bbf2d9942b0d9ced0e39b72cb72c8
armariya <arm.armariya@gmail.com>
2013-08-07 02:49:11 +0700

    first generation has been started :))

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from Town import Town
 from costMatrix import CostMatrix
+from salesMan import SalesMan
 #import matplotlib.pyplot as plt
 
+#initial variable for Genetic Algorithm
+MAX_ITERATION = 1000
+MAX_POPULATION = 20
+PC = 0.7
+PM = 0.05
+
+#create list of object town
 townList = list()
 townData = open("town.txt", 'r')
 while townData:
@@ -11,4 +19,24 @@ while townData:
 	townList.append(Town(line[0], line[1], line[2]))
 
 #createCostMatrix
-costMatrixDict = CostMatrix().createCostMatrix()
+costMatrixDict = CostMatrix(townList).createCostMatrix()
+
+#create population
+populationList = list()
+for i in range(20):
+	populationList.append(SalesMan())
+	populationList[i].randomFirstPath(townList)
+	populationList[i].calculateFitness(costMatrixDict, townList)
+
+#start iteration
+iteration = 1
+
+#crossover population
+
+#mutation population
+
+#get rid of chromosome has lower fitness than the other
+
+#end iteration
+
+#return Answer
--- salesMan.py
+++ salesMan.py
@@ -1,10 +1,35 @@
 import math
+import random
 
-class salesMan():
-	def __init__(self, numberOfPopulation):
+class SalesMan():
+	def __init__(self):
 		#this is fitness
-		self.sumDistance = 0 
+		self.sumDistanceFitness = 0 
+		self.path = list()
 		
+	def randomFirstPath(self, townList):
+		usedToGo = list()
+		while (len(self.path) != len(townList)):
+			nextTown = random.randint(1, 200)
+			if nextTown not in usedToGo:
+				self.path.append(nextTown)
+
+	def randomStartingPoint(self):
+		startingPoint = random.randint(1, 200)
+		self.startingPoint = startingPoint
+
 	def calculateDistanceBetweenPoint(self, x1, y1, x2, y2):
 		distance = math.sqrt(math.pow((x1 - x2), 2) + math.pow((y1-y2), 2))
 		return distance
+
+	def getPath(self):
+		return self.path
+
+	def calculateFitness(self, costMatrixDict, townList):
+		pathIndex = 0
+		while pathIndex <= len(townList) - 2:
+			self.sumDistanceFitness = self.sumDistanceFitness + costMatrixDict[int(self.path[pathIndex])][int(self.path[pathIndex+1])]
+			pathIndex = pathIndex + 1
+
+	def getFitness(self):
+		return self.sumDistanceFitness
